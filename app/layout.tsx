8f1916c6f98cdcc9652471792d3df00e9afdc740
PPiai <166030271+PPiai@users.noreply.github.com>
2025-08-19 10:47:24 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { GeistMono } from "geist/font/mono"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Sales Ops AI",
+  description: "Criado por V4 Ferraz Piai",
+  generator: "v4ferrazpiai.com.br",
 }
 
 export default function RootLayout({
